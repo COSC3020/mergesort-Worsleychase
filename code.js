1781accdc6b0e8534583b34ad6ac920519ff2c34
Chase Worsley <68138593+Worsleychase@users.noreply.github.com>
2025-04-18 22:24:02 -0600

    Make merging happen in place

--- code.js
+++ code.js
@@ -6,50 +6,47 @@ function mergesort(array) {
     width = 1; // width of sub array
     while (width < array.length) {
         // break into many sub arrays and merge
-        for (let i = 0; i < array.length; i += width * 2) {
-            l = i;
-            m = Math.min(i + width, array.length);
-            r = Math.min(i + width * 2, array.length);
-            merge(array, l, m, r);
+        for (let start = 0; start < array.length; start += width * 2) {
+            l = start;
+            m = Math.min(start + width, array.length);
+            r = Math.min(start + width * 2, array.length);
+
+            // divide into two halves
+            lh = array.slice(l, m);
+            rh = array.slice(m, r);
+
+            // init indexes for halves
+            i = l;
+            li = 0;
+            ri = 0;
+
+            // merge when non-empty
+            while (li < lh.length && ri < rh.length) {
+                if (lh[li] <= rh[ri]) {
+                    array[i] = lh[li];
+                    li++;
+                } else {
+                    array[i] = rh[ri];
+                    ri++;
+                }
+                i++;
+            }
+
+            // merge remaining lh
+            while (li < lh.length) {
+                array[i] = lh[li];
+                li++;
+                i++;
+            }
+
+            // merge remaining rh
+            while (ri < rh.length) {
+                array[i] = rh[ri];
+                ri++;
+                i++;
+            }
         }
         width *= 2;
     }
     return array;
-}
-
-function merge(arr, l, m, r) {
-    // divide into two halves
-    lh = arr.slice(l, m);
-    rh = arr.slice(m, r);
-
-    // init indexes for halves
-    i = l;
-    li = 0;
-    ri = 0;
-
-    // merge when non-empty
-    while (li < lh.length && ri < rh.length) {
-        if (lh[li] <= rh[ri]) {
-            arr[i] = lh[li];
-            li++;
-        } else {
-            arr[i] = rh[ri];
-            ri++;
-        }
-        i++;
-    }
-
-    // merge remaining lh
-    while (li < lh.length) {
-        arr[i] = lh[li];
-        li++;
-        i++;
-    }
-
-    // merge remaining rh
-    while (ri < rh.length) {
-        arr[i] = rh[ri];
-        ri++;
-        i++;
-    }
 }
